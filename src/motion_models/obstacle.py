f01ece63f674ce5e1c0948586eb337912958629e
ArunMekkad <mekkad.a@northeastern.edu>
2025-04-03 22:54:59 -0400

    Obstacle.py editing 2

--- src/motion_models/obstacle.py
+++ src/motion_models/obstacle.py
@@ -30,5 +30,51 @@ class Obstacle:
         center_distance = np.linalg.norm(point - self.position)
         return max(0.0, center_distance - self.radius)
     
+    def is_collision(self, point: np.ndarray, safety_margin: float = 0.0) -> bool:
+
+        # Check if the point is in collision with the obstacle
+        return self.distance_to(point) <= safety_margin
     
+    def update(self, dt: float):
+        raise NotImplementedError("Subclasses must implement update()")
+
+class StaticObstacle(Obstacle):
+
+    def update(self, dt: float):
+        pass                        # since no movement
+
+class DynamicObstacle(Obstacle):
+
+    def __init__(self, position: np.ndarray, radius):
+        super().__init__(position, radius)
+        self.movements = [] # Store previous movement vectors
+
+    def get_movements(self) -> List[np.ndarray]:
+        return self.movements
     
+    def move(self, translation: np.ndarray):
+
+        # Move the obstacle by given translation vector
+        self.movements.append(translation.copy())
+        self.position += translation
+        self.trajectory.append(self.position.copy())
+
+class GaussianObstacle(DynamicObstacle):
+    
+    # Obstacle that moves according to a Gaussian Distribution
+
+    def __init__(self, position, radius, mean: np.ndarray, covariance: np.ndarray):
+        super().__init__(position, radius)
+
+        self.mean = np.array(mean, dtype=float)
+        self.covariance = np.array(covariance, dtype=float)
+
+    def update(self, dt: float):
+        scaled_mean = self.mean * dt
+        scaled_covariance = self.covariance * dt
+
+        movement = np.random.multivariate_normal(scaled_mean,scaled_covariance) # Sample a random movement
+        self.move(movement) # Apply the movement
+
+class GMMObstacle(DynamicObstacle):
+    pass
